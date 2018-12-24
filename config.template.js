16631a4cfa73c975dd6920e1902236a3e3912a89
Klervi Corbel <corbel.klervi@gmail.com>
2018-12-24 11:46:40 +0100

    Update config template

--- config.template.js
+++ config.template.js
@@ -1,4 +1,11 @@
 module.exports = {
-  oauth: 'Your token here without oauth:, only what is after !',
-  clientID: 'Your API ClientID'
+  oauth: "Your token here without oauth:, only what is after !",
+  clientID: "Your API ClientID",
+  connection: {
+    host: "localhost",
+    port: 5432,
+    database: "my-database-name",
+    user: "user-name",
+    password: "user-password"
+  }
 };
